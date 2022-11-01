433819dbcb0482bba36a9f719a1ec124d58040b8
Mardonio Costa <mardoniocosta@pjc.mt.gov.br>
2022-11-01 11:30:13 -0400

    feat: Feito o calculo de acordo com o valor informado
    se o numero for um numero par faz calculo de raiz se for impar faz calculo de quadrado

--- src/main/java/com/servicosmsc/MSC/resources/CalculoResource.java
+++ src/main/java/com/servicosmsc/MSC/resources/CalculoResource.java
@@ -1,14 +1,21 @@
 package com.servicosmsc.MSC.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/raiz_quadrado")
 public class CalculoResource {
-	@GetMapping()
-	public String resultado() {
-		return "REST está funcionado";
-	}
+
+  @GetMapping("/{valor}")
+  public String resultado(@PathVariable double valor) {
+    if (valor % 2 == 0) {
+      valor = Math.sqrt(valor);
+      return "A raiz desse número é: " + valor;
+    }
+
+    return "O quadrado desse número é: " + valor * valor;
+  }
 }
